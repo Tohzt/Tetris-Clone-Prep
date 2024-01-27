48cf29c42b7bdaf609b63901051133e55f1e746e
tohzt <steven.g.french@live.com>
2024-01-26 23:41:09 -0700

    scene navigation

---
+++ Scripts/Game_Manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var game_is_paused := false
+
+func _process(_delta):
+	pass
---
+++ Scripts/Game_Script.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var pause_screen = $CanvasLayer/CenterContainer/PauseScreen
+const menu_scene = "res://Scenes/Menu_Scene.tscn"
+
+func _ready():
+	pause_screen.visible = false
+
+func _process(_delta):
+	pass
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("START"):
+		_pause_game()
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file(menu_scene)
+
+func _pause_game() -> void:
+	GameManager.game_is_paused = !GameManager.game_is_paused
+	pause_screen.visible = GameManager.game_is_paused
